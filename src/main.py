d54e0176251368de7783fecee1630f519aa24d90
alexander-carrels <alexander.carrels@outlook.com>
2025-03-31 23:40:55 +0200

    Implement AbstractMusicService

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """Entry point to start the track-back server."""
 
 import os
+from abc import ABC, abstractmethod
 import time
 from datetime import datetime
 from dataclasses import dataclass
@@ -9,12 +10,21 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# ToDo: Allow extension for other music services
+def extract_year(date_str: str) -> int:
+    """Extracts the year from a date string and handles different date formats."""
+    formats = ["%Y-%m-%d", "%Y-%m", "%Y"]
+    for fmt in formats:
+        try:
+            return datetime.strptime(date_str, fmt).year
+        except ValueError:
+            continue
+    raise ValueError(f"Unknown date format: {date_str}")
 
 
 @dataclass
 class Song:
     """Represents a song with title, artist and release year."""
+
     title: str
     artist: str
     release_year: int
@@ -23,8 +33,69 @@ class Song:
         return f"'{self.title}' by {self.artist} ({self.release_year})"
 
 
+class AbstractMusicService(ABC):
+    """Abstract base class for a music service."""
+
+    @abstractmethod
+    def current_song(self) -> Song:
+        """Returns the currently playing song."""
+        pass
+
+    @abstractmethod
+    def start_playback(self):
+        """Starts playing the music."""
+        pass
+
+    @abstractmethod
+    def next_track(self):
+        """Skips to the next track."""
+        pass
+
+
+class SpotifyService(AbstractMusicService):
+    """Interface to the Spotify API."""
+    def __init__(self):
+        self.session = self._initialize_spotify_session()
+
+    def current_song(self) -> Song:
+        playback = self.session.current_playback()
+
+        song_name = playback["item"]["name"]
+        artist_names = ", ".join([artist["name"] for artist in playback["item"]["artists"]])
+        release_year = extract_year(playback["item"]["album"]["release_date"])
+
+        return Song(title=song_name, artist=artist_names, release_year=release_year)
+
+    def start_playback(self):
+        try:
+            self.session.start_playback()
+        except spotipy.exceptions.SpotifyException:
+            print("Probably song is already plaing, skip to next...")
+            try:
+                self.session.next_track()
+            except spotipy.exceptions.SpotifyException:
+                print("Could not start playback. Please start a song manually.")
+                exit(1)
+
+    def next_track(self):
+        return self.session.next_track()
+
+    def _initialize_spotify_session(self):
+        """Initializes a spotify session."""
+
+        o_authenticator = SpotifyOAuth(
+            client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+            client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+            redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+            scope="user-library-read,user-read-playback-state,user-modify-playback-state",
+        )
+
+        return spotipy.Spotify(auth_manager=o_authenticator)
+
+
 class User:
     """Represents a user with a name and a list of songs."""
+
     def __init__(self, name):
         self.name = name
         self.song_list = []
@@ -38,7 +109,7 @@ class User:
         """Gets a valid index from the user by input."""
         while True:
             input_index = input(
-                "\nEnter the index in front of which the song shall be added. " \
+                "\nEnter the index in front of which the song shall be added. "
                 "(0 for the first song, -1 for the last song): "
             )
             try:
@@ -52,6 +123,7 @@ class User:
 
 class TrackBackGame:
     """Handles the game session"""
+
     def __init__(self, users, target_history_length, session):
         self.session = session
         self.users = users
@@ -74,7 +146,7 @@ class TrackBackGame:
         user.print_song_list()
         input_index = user.get_valid_index_by_input()
 
-        current_song = get_current_song(self.session)
+        current_song = self.session.current_song()
 
         if not user.song_list:
             user.song_list.append(current_song)
@@ -104,19 +176,6 @@ class TrackBackGame:
             self.finished = True
 
 
-def initialize_spotify_session():
-    """Initializes a spotify session."""
-
-    o_authenticator = SpotifyOAuth(
-        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
-        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
-        scope="user-library-read,user-read-playback-state,user-modify-playback-state",
-    )
-
-    return spotipy.Spotify(auth_manager=o_authenticator)
-
-
 def get_users():
     """Gets the user names by input."""
     users = []
@@ -132,49 +191,14 @@ def get_users():
     return users
 
 
-def extract_year(date_str: str) -> int:
-    """Extracts the year from a date string and handles different date formats."""
-    formats = ["%Y-%m-%d", "%Y-%m", "%Y"]
-    for fmt in formats:
-        try:
-            return datetime.strptime(date_str, fmt).year
-        except ValueError:
-            continue
-    raise ValueError(f"Unknown date format: {date_str}")
-
-
-def get_current_song(session) -> Song:
-    """Gets the current song from the streaming session."""
-    playback = session.current_playback()
-    song_name = playback["item"]["name"]
-    artist_names = ", ".join([artist["name"] for artist in playback["item"]["artists"]])
-    release_date = playback["item"]["album"]["release_date"]
-    release_year = extract_year(release_date)
-
-    current_song = Song(song_name, artist_names, release_year)
-    return current_song
-
-
-def start_playback(session):
-    """Starts the playback of the session."""
-    try:
-        session.start_playback()
-    except spotipy.exceptions.SpotifyException:
-        print("Probably song is already plaing, skip to next...")
-        try:
-            session.next_track()
-        except spotipy.exceptions.SpotifyException:
-            print("Could not start playback. Please start a song manually.")
-            exit(1)
-
-
 def main():
+    """Main entry point to start the game."""
     load_dotenv()
 
     target_history_length = 2
 
-    session = initialize_spotify_session()
-    start_playback(session)
+    session = SpotifyService()
+    session.start_playback()
 
     users = get_users()
 
