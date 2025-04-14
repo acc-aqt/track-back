1877f9f096842357019be8b0f895d5dd6d4b5146
alexander-carrels <alexander.carrels@outlook.com>
2025-04-14 18:41:04 +0200

    Fix linter errors

--- src/backend/server/local_ip.py
+++ src/backend/server/local_ip.py
@@ -1,28 +1,21 @@
-"""Contains a function to get the local IPv4 address of the machine running the script."""
+"""Get the local IPv4 address of the machine running the script."""
 
 import ipaddress
 import os
 import platform
+import shlex
 import subprocess
 
 
-def _run_shell_command(command: str, capture_output: bool =False) -> None:
-    print(f"$ {command}")
-    return subprocess.run(
-        command,
-        shell=True,
-        capture_output=capture_output,
-        text=capture_output,
-        check=False,
-    )  # 'shell=True' => Use built-in shell, '/bin/sh' on Linux and Mac, 'cmd.exe' on Windows.
+class LocalIpRetrievalError(Exception):
+    """Custom exception for local IP retrieval errors."""
 
 
 def get_local_ip() -> str:
     """Get the local IPv4 address of the machine running the script."""
-    local_ipv4 = None
-
+    local_ipv4 = ""
     operating_system_kind = platform.system()
-    if operating_system_kind == "Darwin":
+    if operating_system_kind == "Darwin":  # macOS
         local_ipv4 = _run_shell_command(
             "ipconfig getifaddr en0", True
         ).stdout.strip()
@@ -37,18 +30,34 @@ def get_local_ip() -> str:
     else:  # Linux
         local_ipv4 = _run_shell_command(
             "hostname -I | awk '{print $1}'", True
-        ).stdout.strip()
+        ).stdout.strip()  # TODO(Alex): not tested under Linux...
 
     try:
         ipaddress.IPv4Address(local_ipv4)
-    except ipaddress.AddressValueError:
-        raise Exception(
+    except ipaddress.AddressValueError as e:
+        raise LocalIpRetrievalError(
             "Could not get local network IPv4 address for Expo Fronted"
-        )
+        ) from e
 
     if local_ipv4.startswith("127.0."):
-        raise Exception(
-            f"Could not get correct local network IPv4 address for Expo Fronted, got {local_ipv4}"
+        raise LocalIpRetrievalError(
+            "Could not get correct local network IPv4 address"
+            "for Expo Fronted, got {local_ipv4}"
         )
 
     return local_ipv4
+
+
+def _run_shell_command(
+    command: str, capture_output: bool = False
+) -> subprocess.CompletedProcess[str]:
+    print(f"Running command: {command}")
+
+    # ruff: noqa: S603
+    completed_process = subprocess.run(
+        shlex.split(command),
+        capture_output=capture_output,
+        text=capture_output,
+        check=False,
+    )
+    return completed_process
