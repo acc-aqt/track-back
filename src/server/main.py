f999930a04f517d61c77fed0a7ffea50021f4e8d
alexander-carrels <alexander.carrels@outlook.com>
2025-04-30 08:31:01 +0200

    Remove local_ip.py

--- src/server/main.py
+++ src/server/main.py
@@ -4,7 +4,6 @@ import argparse
 import logging
 import os
 
-from server.local_ip import get_local_ip
 from server.server import Server
 
 
@@ -20,25 +19,12 @@ def parse_args() -> int:
     return port
 
 
-def log_server_info(port: int) -> None:
-    """Log info where the server is running."""
-    if os.getenv("RENDER") == "true":
-        logging.info("Running on Render")
-    else:
-        logging.info("Running locally")
-        ip = get_local_ip()
-        url = f"http://{ip}:{port}"
-        logging.info("\nGame server running at: %s\n", url)
-
-
 def main() -> None:
     """Parse args and start the server."""
     logging.basicConfig(level=logging.INFO)
 
     port = parse_args()
     server = Server()
-
-    log_server_info(port)
 
     server.run(port=port)
 
